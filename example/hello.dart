2c2438fb2a3c7c8e28554f5dd778faa20e99ac7f
Aldi Prasedya <perasedya18@gmail.com>
2022-06-20 11:35:39 +0700

    git config --global user.email "perasedya18@gmail.com" 			git config --global user.name "Aldi Prasedya"

---
+++ example/hello.dart
@@ -0,0 +1,10 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main(){
+
+  print(sayHello("Aldi"));
+
+  var customer = Customer();
+  var category = Category();
+  var product = Product();
+}
